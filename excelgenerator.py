8c170d1e666d41afd6214838e33262e759919906
SniperJF <Jorge.FonsecaCacho@UNLV.edu>
2022-08-12 15:59:24 -0700

    Adding a new table of all drug trials in one
    will be useful for generating charts

--- excelgenerator.py
+++ excelgenerator.py
@@ -3,7 +3,7 @@ import xlsxwriter #Installed using pip install XlsxWriter. See: https://xlsxwrit
 import time #For versioning our file
 def generateExcelDemographics(demogHeadings, demogTable):
     #First Let's split data into tables we want: T0: US-ONLY Drug, T1: NON-US Drug, T2: GLOBAL Drug, T3: NONE Drug, 
-    #T4: Biomarker, Device, Other (in that order), and T5: Master Data (Original version of Data as it is in demogTable)
+    #T4: Biomarker, Device, Other (in that order), T5 all Drug trials and T6: Master Data (Original version of Data as it is in demogTable)
     finalTables = DemogTableSplitter(demogTable)
 
     #Next let's update the top headings and generate individual headings for each table
@@ -27,13 +27,15 @@ def generateExcelDemographics(demogHeadings, demogTable):
 
     rowCounter = 2 #Starts at 2 since we already printed the title at row 0 and left empty space at row 1
     for row in demogHeadings[:-3]:
-        if rowCounter % 2 == 0 and rowCounter < 22:
+        if rowCounter < 13:
             cell_format = workbook.add_format({'bold': True})
-        elif rowCounter == 24 or rowCounter == 29 or rowCounter == 34: #Hardcoded :o
+            worksheet.write(rowCounter, 0, row[0], cell_format)
+            worksheet.write(rowCounter, 1, row[1])
+        elif rowCounter == 15 or rowCounter == 20 or rowCounter == 25: #Hardcoded :o
             cell_format = workbook.add_format({'bold': True, 'border': 1, 'bg_color': '#E9FF97'})
+            worksheet.write_row(rowCounter, 0, row, cell_format) #Write Headings
         else:
-            cell_format = workbook.add_format()
-        worksheet.write_row(rowCounter, 0, row, cell_format) #Write Headings
+            worksheet.write_row(rowCounter, 0, row) #Write Headings
         rowCounter +=1
 
     rowCounter +=3
@@ -41,10 +43,8 @@ def generateExcelDemographics(demogHeadings, demogTable):
     for table in finalTables:
         merge_format = workbook.add_format({'bold': True, 'bg_color': '#B8B8B8', 'border': 1, 'font_size': 22,
                                             'align': 'center', 'valign': 'vcenter'}) #Set header formatting
-        if i!=5: worksheet.merge_range(rowCounter,0, rowCounter, 26, demogHeadings[i+i+4][0], merge_format)
-        else: worksheet.merge_range(rowCounter,0, rowCounter, 26, demogHeadings[2][0], merge_format)
-        #if i!=5: worksheet.write_row(rowCounter, 0, demogHeadings[i+i+4], cell_format) #Write Headings
-        #else: worksheet.write_row(rowCounter, 0, demogHeadings[2]) #due to order
+        if i!=6: worksheet.merge_range(rowCounter,0, rowCounter, 26, demogHeadings[i+2][1], merge_format)
+        else: worksheet.merge_range(rowCounter,0, rowCounter, 26, demogHeadings[1][1], merge_format)
         rowCounter +=1       
         for row in individualTableHeadings[i]:
             cell_format = workbook.add_format({'bold': True, 'bg_color': '#96DADA', 'border': 1}) #Set header formatting
@@ -68,9 +68,10 @@ def generateExcelDemographics(demogHeadings, demogTable):
 #T2: GLOBAL Drug
 #T3: NONE Drug
 #T4: Biomarker, Device, Other (in that order)
-#T5: Master Data (Original version of Data as it is in demogTable)
+#T5: All Drug Trials
+#T6: Master Data (Original version of Data as it is in demogTable)
 def DemogTableSplitter(demogTable): #row[22] Country Type, #row[24] #Trial Type
-    finalTables = [ [],[],[],[],[],[] ] #T0-T5
+    finalTables = [ [],[],[],[],[],[],[] ] #T0-T6
     for row in demogTable:
         if row[24] == 'Drug':
             if row[22] == 'US-ONLY':
@@ -83,26 +84,23 @@ def DemogTableSplitter(demogTable): #row[22] Country Type, #row[24] #Trial Type
                 finalTables[3].append(row)                
             else:
                 print("Warning: Excel File Did Not Write Row Due to bad Class: ", row[0], row[23])
+            finalTables[5].append(row) #All Drug Trials (useful to make graphics for all drug trials)
         else: #T4
             finalTables[4].append(row)
     finalTables[4].sort(key=lambda row: (row[24], row[0]), reverse=False) #Stable Sort T4
-    finalTables[5] = demogTable #T5 is same as start so just throw a ref to it
+    finalTables[6] = demogTable #T5 is same as start so just throw a ref to it
     return finalTables
 #End Function
 
 #Updates demogHeadings and also creates individual headings to go on our final tables (label and totals)
 def demographicHeadingUpdater(demogHeadings, finalTables):
     #First let's add new totals at the top. That is insert after row[3]
-    demogHeadings.insert(4,  ["Total AD/MCI Drug Trials with Results in US-ONLY:"])
-    demogHeadings.insert(5,  [len(finalTables[0])])
-    demogHeadings.insert(6,  ["Total AD/MCI Drug Trials with Results in NON-US:"])
-    demogHeadings.insert(7,  [len(finalTables[1])])
-    demogHeadings.insert(8,  ["Total AD/MCI Drug Trials with Results in GLOBAL:"])
-    demogHeadings.insert(9,  [len(finalTables[2])])
-    demogHeadings.insert(10, ["Total AD/MCI Drug Trials with Results with No Country Data:"])
-    demogHeadings.insert(11, [len(finalTables[3])])
-    demogHeadings.insert(12, ["Total AD/MCI Non-Drug Trials with Results:"])
-    demogHeadings.insert(13, [len(finalTables[4])])
+    demogHeadings.insert(4, [len(finalTables[0]),"Total AD/MCI Drug Trials with Results in US-ONLY"])
+    demogHeadings.insert(5, [len(finalTables[1]),"Total AD/MCI Drug Trials with Results in NON-US"])
+    demogHeadings.insert(6, [len(finalTables[2]),"Total AD/MCI Drug Trials with Results in GLOBAL"])
+    demogHeadings.insert(7, [len(finalTables[3]),"Total AD/MCI Drug Trials with Results with No Country Data"])
+    demogHeadings.insert(8, [len(finalTables[4]),"Total AD/MCI Non-Drug Trials with Results"])
+    demogHeadings.insert(9, [len(finalTables[5]),"Total AD/MCI All Drug Trials with Results"])
     biomarkerCount = 0
     deviceCount = 0
     otherCount = 0
@@ -110,12 +108,9 @@ def demographicHeadingUpdater(demogHeadings, finalTables):
         if   i[24] == "Biomarker": biomarkerCount += 1
         elif i[24] == "Device": deviceCount += 1
         else: otherCount += 1
-    demogHeadings.insert(14, ["Total AD/MCI Biomarker Trials with Results:"])
-    demogHeadings.insert(15, [biomarkerCount])
-    demogHeadings.insert(16, ["Total AD/MCI Device Trials with Results:"])
-    demogHeadings.insert(17, [deviceCount])
-    demogHeadings.insert(18, ["Total AD/MCI Others Trials with Results:"])
-    demogHeadings.insert(19, [otherCount])
+    demogHeadings.insert(10, [biomarkerCount,"Total AD/MCI Biomarker Trials with Results"])
+    demogHeadings.insert(11, [deviceCount,"Total AD/MCI Device Trials with Results"])
+    demogHeadings.insert(12, [otherCount,"Total AD/MCI Others Trials with Results"])
     #Next let's add each table's individual headings
     individualTableHeadings = []
     for i in range(len(finalTables)): #Final Table Headings is already done
@@ -135,5 +130,10 @@ def demographicHeadingUpdater(demogHeadings, finalTables):
                         exit()
             footerRow.append(colsum)
         footerTotals.append(footerRow)
+    #One last thing lets combine row 0,1 and 2,3 to match format
+    demogHeadings[0] = [demogHeadings[1][0], demogHeadings[0][0]]
+    demogHeadings[2] = [demogHeadings[3][0], demogHeadings[2][0]]
+    demogHeadings.pop(3)
+    demogHeadings.pop(1)
     return individualTableHeadings, footerTotals
 #End File
